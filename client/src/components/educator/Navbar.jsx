ba84e8920f6e7309eed2dbd5b661a045a8de7564
TejaThondrothu <thondrothuteja1997@gmail.com>
2025-02-13 14:13:08 +0530

    feat: Initial project setup for LMS application
    - Set up project structure with required components, pages, and routing
    - Placed required images in the ssets folder and created ssets.js for dynamic data
    - Installed necessary npm packages for development
    - Configured Tailwind CSS for styling
    - Integrated Clerk authentication for Sign In functionality
    - Developed Homepage components:
      - Navbar
      - Hero Section
      - CourseCard
      - Course Section
      - Search Bar

---
+++ client/src/components/educator/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>EDU-Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useEffect, useState } from "react";
+import { assets, dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props)=>{
+
+    const currency = import.meta.env.VITE_CURRENCY;
+    const navigate = useNavigate();
+
+    const [allCourses,setAllCourses] =useState([]);
+    const [isEducator,setIsEducator]=useState(true);
+
+    //fetch all the courses
+    const fetchAllCourses = async ()=>{
+        setAllCourses(dummyCourses);
+    }
+    //function to calculat average rating of the course
+
+    const calculateRatings =(course)=>{
+        if(course.courseRatings.length ===0){
+            return 0;
+        }
+        let totalRating =0;
+        course.courseRatings.forEach(rating=>{
+            totalRating+=rating.rating;
+        })
+        return totalRating/course.courseRatings.length;
+    }
+
+    useEffect(()=>{
+        fetchAllCourses();
+    },[])
+
+    const value={
+        currency,allCourses,navigate,calculateRatings,isEducator,setIsEducator
+    };
+
+    return(
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    );
+}
